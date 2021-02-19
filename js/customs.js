56dba714133e9cadd3fea2120fcfe0612b2f849f
fl0w <officialdeltadev@gmail.com>
2021-02-19 13:03:26 -0500

    Animation changes. Temp feedback code.

--- js/customs.js
+++ js/customs.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
 // THIS CODE WAS NOT WRITTEN BY ME!
 // Credits: https://stackoverflow.com/questions/27747970/how-to-make-images-fade-visible-when-im-scrolling/27748638
-function showImages(el) {
+function fadeIn(el) {
   var windowHeight = jQuery( window ).height();
   $(el).each(function(){
     var thisPos = $(this).offset().top;
@@ -21,12 +21,40 @@ function showImages(el) {
   });
 }
 
+function fadeLeft(el) {
+  var windowHeight = jQuery( window ).height();
+  $(el).each(function(){
+    var thisPos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (topOfWindow + windowHeight - 200 > thisPos ) {
+      $(this).addClass("fadeLeft");
+    }
+  });
+}
+
+function fadeRight(el) {
+  var windowHeight = jQuery( window ).height();
+  $(el).each(function(){
+    var thisPos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (topOfWindow + windowHeight - 200 > thisPos ) {
+      $(this).addClass("fadeRight");
+    }
+  });
+}
+
 // if the image in the window of browser when the page is loaded, show that image
 $(document).ready(function(){
-    showImages('.star');
+    fadeIn('.star');
+    fadeLeft('.fadeLeft');
+    fadeRight('.fadeRight');
 });
 
 // if the image in the window of browser when scrolling the page, show that image
 $(window).scroll(function() {
-    showImages('.star');
+    fadeIn('.star');
+    fadeLeft('.fadeLeft');
+    fadeRight('.fadeRight');
 });
